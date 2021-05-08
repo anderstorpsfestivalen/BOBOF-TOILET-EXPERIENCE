09882491d185808f407b8333d8262162f4b23c7f
coral <jonas@jonasbengtson.se>
2021-05-08 16:59:01 -0700

    denis

--- src/patterns.h
+++ src/patterns.h
@@ -1,12 +1,10 @@
-// #include "settings.h"
-// #include <FastLED.h>
+#include "settings.h"
+#include <FastLED.h>
 
-// void init();
 
-// void rainbow();
 
-// extern CRGB leds[];
-
-// class BOBOFLamp {
-
-// }
+class BOBOFLamp {
+    public:
+        CRGB leds[NUM_LEDS];
+        FastLED.addLeds<APA102, 11, 13, BGR>(leds, NUM_LEDS)
+};
